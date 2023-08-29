132458903bb636c8a54b06473594a0ea94887df8
enming <zhangenming@entropyreduce.com>
2023-08-29 12:02:23 +0800

    feat: 增加枚举定义

---
+++ src/constant/PromptEnum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+# prompt 类型枚举
+class PromptEnum(Enum):
+    Chat = "chatPrompt"
+    Summary = "summaryPrompt"
---
+++ src/constant/RoleEnum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+# 角色枚举
+class RoleEnum(Enum):
+    Human = 1
+    Ai = 2
+    System = 2
